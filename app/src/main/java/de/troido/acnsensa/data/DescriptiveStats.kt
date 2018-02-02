716f78b9fe2111c1f4794df81a829c2623daa478
borna-mdn <borna@simvelop.de>
2018-02-02 15:12:07 +0100

    Delete all code

--- app/src/main/java/de/troido/acnsensa/data/DescriptiveStats.kt
+++
@@ -1,3 +0,0 @@
-package de.troido.acnsensa.data
-
-data class DescriptiveStats<out T>(val min: T, val max: T, val avg: T)
--- app/src/main/java/de/troido/acnsensa/data/Vector3F.kt
+++
@@ -1,3 +0,0 @@
-package de.troido.acnsensa.data
-
-data class Vector3F(val x: Float, val y: Float, val z: Float)
