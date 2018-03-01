afe8eb255e60e320cbf840bab17205f791ea9d80
Sergio Mauricio Rodriguez Barrientos <sergio@simvelop.de>
2018-03-01 17:24:21 +0100

    Replace log calls with timber in BluetoothScanningViewModel.

--- app/src/main/java/com/aconno/acnsensa/BluetoothScanningViewModel.kt
+++ app/src/main/java/com/aconno/acnsensa/BluetoothScanningViewModel.kt
@@ -4,10 +4,10 @@ import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
 import android.content.Intent
 import android.support.v4.content.LocalBroadcastManager
-import android.util.Log
 import com.aconno.acnsensa.domain.Bluetooth
 import com.aconno.acnsensa.domain.model.ScanEvent
 import io.reactivex.Flowable
+import timber.log.Timber
 
 //TODO: This needs refactoring.
 /**
@@ -29,14 +29,12 @@ class BluetoothScanningViewModel(
     }
 
     fun startScanning() {
-        Log.e("ACNSENSA", "startScanning")
-
-
+        Timber.d("startScanning")
         BluetoothScanningService.start(getApplication())
     }
 
     fun stopScanning() {
-        Log.e("ACNSENSA", "stopScanning")
+        Timber.d("stopScanning")
 
         val localBroadcastManager = LocalBroadcastManager.getInstance(getApplication())
         localBroadcastManager.sendBroadcast(Intent("com.aconno.acnsensa.STOP"))
@@ -46,6 +44,3 @@ class BluetoothScanningViewModel(
         return result
     }
 }
-
-
-
