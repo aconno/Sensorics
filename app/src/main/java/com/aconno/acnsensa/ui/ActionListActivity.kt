96fc9a4327cefce94b4d35819cd771069c9eb967
Sergio Mauricio Rodriguez Barrientos <sergio@simvelop.de>
2018-03-23 10:32:33 +0100

    Include initial code for ifttt ui

---
+++ app/src/main/java/com/aconno/acnsensa/ui/ActionListActivity.kt
@@ -0,0 +1,23 @@
+package com.aconno.acnsensa.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import com.aconno.acnsensa.R
+
+class ActionListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_action_list)
+
+        val fm = supportFragmentManager
+        if (fm != null) {
+            var fragment: Fragment? = fm.findFragmentById(R.id.action_list_container)
+            if (fragment == null) {
+                fragment = ActionListFragment.newInstance("", "")
+                fm.beginTransaction().add(R.id.action_list_container, fragment).commit()
+            }
+        }
+    }
+}
