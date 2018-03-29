2d2fce99c1a80bea175453b0d777351af7bfc230
Sergio Mauricio Rodriguez Barrientos <sergio@simvelop.de>
2018-03-29 13:11:49 +0200

    Remove unused logic in NotificationOutcome.

--- domain/src/main/java/com/aconno/acnsensa/domain/ifttt/NotificationOutcome.kt
+++ domain/src/main/java/com/aconno/acnsensa/domain/ifttt/NotificationOutcome.kt
@@ -7,24 +7,7 @@ class NotificationOutcome(
     val message: String,
     private val notificationDisplay: NotificationDisplay
 ) : Outcome {
-
-    //TODO: Remove logic
-
-    val time = 10_000
     override fun execute() {
-        if (!running) {
-            running = true
-            val now = System.currentTimeMillis()
-            notificationDisplay.displayAlertNotification(message)
-            while (System.currentTimeMillis() - now < time) {
-                Thread.sleep(100)
-            }
-
-            running = false
-        }
-    }
-
-    companion object {
-        var running = false
+        notificationDisplay.displayAlertNotification(message)
     }
 }
