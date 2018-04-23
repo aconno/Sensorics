537ac6c3e7136f11430bb26505ede956da9d44d7
Sergio Mauricio Rodriguez Barrientos <sergio@simvelop.de>
2018-04-23 11:32:10 +0200

    Flush the textToSpeech queue.

--- device/src/main/java/com/aconno/acnsensa/device/TextToSpeechPlayerImpl.kt
+++ device/src/main/java/com/aconno/acnsensa/device/TextToSpeechPlayerImpl.kt
@@ -4,19 +4,18 @@ import android.content.Context
 import android.speech.tts.TextToSpeech
 import com.aconno.acnsensa.domain.ifttt.TextToSpeechPlayer
 
-class TextToSpeechPlayerImpl(private val context: Context) :
+class TextToSpeechPlayerImpl(context: Context) :
     TextToSpeechPlayer, TextToSpeech.OnInitListener {
 
-    private lateinit var text: String
-    private lateinit var textToSpeech: TextToSpeech
+    private val textToSpeech = TextToSpeech(context, this)
 
     override fun play(text: String) {
-        this.text = text
-        textToSpeech = TextToSpeech(context, this)
+        if (!textToSpeech.isSpeaking) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "speak")
+        }
     }
 
     override fun onInit(status: Int) {
-        textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, null, "speak")
-        //textToSpeech.shutdown()
+        //Do nothing.
     }
 }
