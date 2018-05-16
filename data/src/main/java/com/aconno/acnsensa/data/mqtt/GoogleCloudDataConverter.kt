3d08fb46569a9ba085e4ed475915ed73fcade329
borna-mdn <borna@simvelop.de>
2018-05-16 14:03:28 +0200

    Change mqtt message format

--- data/src/main/java/com/aconno/acnsensa/data/mqtt/GoogleCloudDataConverter.kt
+++ data/src/main/java/com/aconno/acnsensa/data/mqtt/GoogleCloudDataConverter.kt
@@ -21,46 +21,46 @@ object GoogleCloudDataConverter {
     }
 
     private fun convertTemperature(reading: Reading): List<String> {
-        return listOf("Temperature,Result,${reading.values[0]},Celcius,${reading.timestamp}")
+        return listOf("Temperature,${reading.values[0]},${reading.timestamp}")
     }
 
     private fun convertLight(reading: Reading): List<String> {
-        return listOf("Light,Result,${reading.values[0]},%,${reading.timestamp}")
+        return listOf("Light,${reading.values[0]},${reading.timestamp}")
     }
 
     private fun convertHumidity(reading: Reading): List<String> {
-        return listOf("Humidity,Result,${reading.values[0]},%,${reading.timestamp}")
+        return listOf("Humidity,${reading.values[0]},${reading.timestamp}")
     }
 
     private fun convertPressure(reading: Reading): List<String> {
-        return listOf("Pressure,Result,${reading.values[0]},hPa,${reading.timestamp}")
+        return listOf("Pressure,${reading.values[0]},${reading.timestamp}")
     }
 
     private fun convertMagnetometer(reading: Reading): List<String> {
         return listOf(
-            "Magnetometer X,Result,${reading.values[0]},uT,${reading.timestamp}",
-            "Magnetometer Y,Result,${reading.values[1]},uT,${reading.timestamp}",
-            "Magnetometer Z,Result,${reading.values[2]},uT,${reading.timestamp}"
+            "Magnetometer X,${reading.values[0]},${reading.timestamp}",
+            "Magnetometer Y,${reading.values[1]},${reading.timestamp}",
+            "Magnetometer Z,${reading.values[2]},${reading.timestamp}"
         )
     }
 
     private fun convertAccelerometer(reading: Reading): List<String> {
         return listOf(
-            "Accelerometer X,Result,${reading.values[0]},uT,${reading.timestamp}",
-            "Accelerometer Y,Result,${reading.values[1]},uT,${reading.timestamp}",
-            "Accelerometer Z,Result,${reading.values[2]},uT,${reading.timestamp}"
+            "Accelerometer X,${reading.values[0]},${reading.timestamp}",
+            "Accelerometer Y,${reading.values[1]},${reading.timestamp}",
+            "Accelerometer Z,${reading.values[2]},${reading.timestamp}"
         )
     }
 
     private fun generateGyroscopeMessages(reading: Reading): List<String> {
         return listOf(
-            "Gyroscope X,Result,${reading.values[0]},uT,${reading.timestamp}",
-            "Gyroscope Y,Result,${reading.values[1]},uT,${reading.timestamp}",
-            "Gyroscope Z,Result,${reading.values[2]},uT,${reading.timestamp}"
+            "Gyroscope X,${reading.values[0]},${reading.timestamp}",
+            "Gyroscope Y,${reading.values[1]},${reading.timestamp}",
+            "Gyroscope Z,${reading.values[2]},${reading.timestamp}"
         )
     }
 
     private fun generateBatteryMesssage(reading: Reading): List<String> {
-        return listOf("Battery Level,${reading.values[0]},%,${reading.timestamp}")
+        return listOf("Battery Level,${reading.values[0]},${reading.timestamp}")
     }
 }
