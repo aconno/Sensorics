97e7473256380304cb84598c58308284da0ca171
borna-mdn <borna@simvelop.de>
2018-05-28 15:57:03 +0200

    Implement BeaconListFragment

--- app/src/main/java/com/aconno/acnsensa/ui/beacons/BeaconListFragment.kt
+++ app/src/main/java/com/aconno/acnsensa/ui/beacons/BeaconListFragment.kt
@@ -1,8 +1,37 @@
 package com.aconno.acnsensa.ui.beacons
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aconno.acnsensa.R
+import com.aconno.acnsensa.adapter.BeaconAdapter
+import com.aconno.acnsensa.adapter.ItemClickListener
+import com.aconno.acnsensa.domain.model.Device
+import kotlinx.android.synthetic.main.fragment_beacon_list.*
 
-class BeaconListFragment : Fragment() {
+class BeaconListFragment : Fragment(), ItemClickListener<Device> {
+
+    private lateinit var beaconAdapter: BeaconAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_beacon_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        beaconAdapter = BeaconAdapter(mutableListOf(), this)
+        beacon_list.adapter = beaconAdapter
+    }
+
+    override fun onItemClick(item: Device) {
+        //TODO: Open main activity with scanning filter by device
+    }
 
     companion object {
 
