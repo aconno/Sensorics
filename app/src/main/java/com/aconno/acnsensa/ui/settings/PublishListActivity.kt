d3cdbcee9e082e2dc4ecc58fd1e8c5a9bbf75eea
Serhat <SerhatSurguvec@gmail.com>
2018-05-28 10:05:21 +0200

    Views Added

---
+++ app/src/main/java/com/aconno/acnsensa/ui/settings/PublishListActivity.kt
@@ -0,0 +1,68 @@
+package com.aconno.acnsensa.ui.settings
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.aconno.acnsensa.R
+import com.aconno.acnsensa.domain.ifttt.BasePublish
+import kotlinx.android.synthetic.main.activity_add_publish.*
+
+
+/**
+ * @author aconno
+ */
+class PublishListActivity : AppCompatActivity(),
+    PublishFragment.OnListFragmentInteractionListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publish_list)
+
+        val fm = supportFragmentManager
+        if (fm != null) {
+            var fragment: Fragment? = fm.findFragmentById(R.id.publish_list_container)
+            if (fragment == null) {
+                fragment = PublishFragment.newInstance()
+                fm.beginTransaction().add(R.id.publish_list_container, fragment).commit()
+            }
+        }
+
+        setSupportActionBar(custom_toolbar)
+    }
+
+    override fun onListFragmentInteraction(item: BasePublish?) {
+        //TODO SendData
+        AddPublishActivity.start(this, item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.publish_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id: Int? = item?.itemId
+        when (id) {
+        //TODO Add startActivityForResult
+            R.id.action_publish_add -> AddPublishActivity.start(
+                this
+            )
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, PublishListActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/aconno/acnsensa/ui/settings/PublishRecyclerViewAdapter.kt
@@ -0,0 +1,81 @@
+package com.aconno.acnsensa.ui.settings
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Switch
+import android.widget.TextView
+import com.aconno.acnsensa.R
+import com.aconno.acnsensa.domain.ifttt.BasePublish
+import com.aconno.acnsensa.domain.ifttt.GooglePublish
+
+
+import com.aconno.acnsensa.ui.settings.PublishFragment.OnListFragmentInteractionListener
+
+import kotlinx.android.synthetic.main.item_publish.view.*
+
+/**
+ * [RecyclerView.Adapter] that can display a [GooglePublish] and makes a call to the
+ * specified [OnListFragmentInteractionListener].
+ */
+class PublishRecyclerViewAdapter(
+    private val mValues: List<BasePublish>,
+    private val mListener: OnListFragmentInteractionListener?,
+    private val mCheckedChangeListener: OnCheckedChangeListener?
+) : RecyclerView.Adapter<PublishRecyclerViewAdapter.ViewHolder>() {
+
+    private val mOnClickListener: View.OnClickListener
+
+    init {
+        mOnClickListener = View.OnClickListener { v ->
+            val item = v.tag as GooglePublish
+            // Notify the active callbacks interface (the activity, if the fragment is attached to
+            // one) that an item has been selected.
+            mListener?.onListFragmentInteraction(item)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_publish, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = mValues[position]
+        holder.mNameView.text = item.name
+        holder.mEnableView.isChecked = item.enabled
+
+        if (item is GooglePublish) {
+            holder.mImageView.setImageResource(R.drawable.google_logo)
+        }
+
+        with(holder.mView) {
+            tag = item
+            setOnClickListener(mOnClickListener)
+        }
+
+        holder.mEnableView.setOnCheckedChangeListener({ _, isChecked ->
+            mCheckedChangeListener?.onCheckedChange(isChecked, position)
+        })
+    }
+
+    override fun getItemCount(): Int = mValues.size
+
+    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val mNameView: TextView = mView.publish_name
+        val mEnableView: Switch = mView.publish_switch
+        val mImageView: ImageView = mView.publish_image
+
+        override fun toString(): String {
+            return super.toString() + " '" + mEnableView.text + "'"
+        }
+    }
+
+    interface OnCheckedChangeListener {
+        fun onCheckedChange(checked: Boolean, position: Int)
+    }
+
+}
