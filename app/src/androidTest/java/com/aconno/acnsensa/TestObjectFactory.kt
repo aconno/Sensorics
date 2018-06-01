b320db07b4d0b9229d74e82ba9e6799bd3864f59
Serhat <SerhatSurguvec@gmail.com>
2018-06-01 15:22:28 +0200

    Update TestObjectFactory and TestViewModelFactory

--- app/src/androidTest/java/com/aconno/acnsensa/TestObjectFactory.kt
+++ app/src/androidTest/java/com/aconno/acnsensa/TestObjectFactory.kt
@@ -1,8 +1,19 @@
 package com.aconno.acnsensa
 
+import com.aconno.acnsensa.data.mapper.GooglePublishDataMapper
+import com.aconno.acnsensa.data.mapper.GooglePublishEntityDataMapper
+import com.aconno.acnsensa.data.mapper.RESTPublishDataMapper
+import com.aconno.acnsensa.data.mapper.RESTPublishEntityDataMapper
+import com.aconno.acnsensa.data.repository.AcnSensaDatabase
+import com.aconno.acnsensa.data.repository.GooglePublishRepositoryImpl
+import com.aconno.acnsensa.data.repository.RESTPublishRepositoryImpl
+import com.aconno.acnsensa.domain.ifttt.GooglePublishRepository
+import com.aconno.acnsensa.domain.ifttt.RESTPublishRepository
+import com.aconno.acnsensa.domain.interactor.ifttt.*
 import com.aconno.acnsensa.model.GooglePublishModel
+import com.aconno.acnsensa.model.RESTPublishModel
 
-class ObjectFactory {
+class TestObjectFactory {
 
     companion object {
 
@@ -15,12 +26,91 @@ class ObjectFactory {
                 "Region",
                 "DeviceRegistry",
                 "Device",
-                "PrivateKey",
+                "Key",
                 false,
                 "Secs",
                 45000L,
                 0L
             )
+        }
+
+        fun makeGooglePublishModelwithGivenId(id: Long): GooglePublishModel {
+
+            return GooglePublishModel(
+                id,
+                "TestUpdate",
+                "TestProjectid",
+                "Region",
+                "DeviceRegistry",
+                "Device",
+                "Key",
+                false,
+                "Secs",
+                45000L,
+                0L
+            )
+        }
+
+        fun makeRESTPublishModel(): RESTPublishModel {
+
+            return RESTPublishModel(
+                0L,
+                "Test",
+                "url",
+                "method",
+                false,
+                "Secs",
+                45000L,
+                0L
+            )
+        }
+
+        fun getGooglePublishRepository(acnSensaDatabase: AcnSensaDatabase): GooglePublishRepository {
+            return GooglePublishRepositoryImpl(
+                acnSensaDatabase.googlePublishDao(),
+                GooglePublishEntityDataMapper(),
+                GooglePublishDataMapper()
+            )
+        }
+
+        fun getRESTPublishRepository(acnSensaDatabase: AcnSensaDatabase): RESTPublishRepository {
+            return RESTPublishRepositoryImpl(
+                acnSensaDatabase.restPublishDao(),
+                RESTPublishEntityDataMapper(),
+                RESTPublishDataMapper()
+            )
+        }
+
+        fun getAddGooglePublishUseCase(googlePublishRepository: GooglePublishRepository): AddGooglePublishUseCase {
+            return AddGooglePublishUseCase(googlePublishRepository)
+        }
+
+        fun getAddRESTPublishUseCase(restPublishRepository: RESTPublishRepository): AddRESTPublishUseCase {
+            return AddRESTPublishUseCase(restPublishRepository)
+        }
+
+        fun getUpdateGooglePublishUseCase(googlePublishRepository: GooglePublishRepository): UpdateGooglePublishUseCase {
+            return UpdateGooglePublishUseCase(googlePublishRepository)
+        }
+
+        fun getUpdateRESTPublishUseCase(restPublishRepository: RESTPublishRepository): UpdateRESTPublishUserCase {
+            return UpdateRESTPublishUserCase(restPublishRepository)
+        }
+
+        fun getAllGooglePublishUseCase(googlePublishRepository: GooglePublishRepository): GetAllGooglePublishUseCase {
+            return GetAllGooglePublishUseCase(googlePublishRepository)
+        }
+
+        fun getAllRESTPublishUseCase(restPublishRepository: RESTPublishRepository): GetAllRESTPublishUseCase {
+            return GetAllRESTPublishUseCase(restPublishRepository)
+        }
+
+        fun getAllEnabledGooglePublishUseCase(googlePublishRepository: GooglePublishRepository): GetAllEnabledGooglePublishUseCase {
+            return GetAllEnabledGooglePublishUseCase(googlePublishRepository)
+        }
+
+        fun getAllEnabledRESTPublishUseCase(restPublishRepository: RESTPublishRepository): GetAllEnabledRESTPublishUseCase {
+            return GetAllEnabledRESTPublishUseCase(restPublishRepository)
         }
     }
 
--- app/src/androidTest/java/com/aconno/acnsensa/TestViewModelFactory.kt
+++ app/src/androidTest/java/com/aconno/acnsensa/TestViewModelFactory.kt
@@ -1,2 +1,48 @@
 package com.aconno.acnsensa
 
+import com.aconno.acnsensa.data.repository.AcnSensaDatabase
+import com.aconno.acnsensa.model.mapper.GooglePublishDataMapper
+import com.aconno.acnsensa.model.mapper.GooglePublishModelDataMapper
+import com.aconno.acnsensa.model.mapper.RESTPublishDataMapper
+import com.aconno.acnsensa.model.mapper.RESTPublishModelDataMapper
+import com.aconno.acnsensa.viewmodel.PublishListViewModel
+import com.aconno.acnsensa.viewmodel.PublishViewModel
+
+class TestViewModelFactory {
+
+    companion object {
+
+        fun getPublishViewModel(acnSensaDatabase: AcnSensaDatabase): PublishViewModel {
+
+            val googlePublishRepository =
+                TestObjectFactory.getGooglePublishRepository(acnSensaDatabase)
+            val restPublishRepository = TestObjectFactory.getRESTPublishRepository(acnSensaDatabase)
+
+            return PublishViewModel(
+                TestObjectFactory.getAddGooglePublishUseCase(googlePublishRepository),
+                TestObjectFactory.getAddRESTPublishUseCase(restPublishRepository),
+                TestObjectFactory.getUpdateGooglePublishUseCase(googlePublishRepository),
+                TestObjectFactory.getUpdateRESTPublishUseCase(restPublishRepository),
+                GooglePublishModelDataMapper(),
+                RESTPublishModelDataMapper()
+            )
+        }
+
+        fun getPublishListViewModel(acnSensaDatabase: AcnSensaDatabase): PublishListViewModel {
+            val googlePublishRepository =
+                TestObjectFactory.getGooglePublishRepository(acnSensaDatabase)
+            val restPublishRepository = TestObjectFactory.getRESTPublishRepository(acnSensaDatabase)
+
+            return PublishListViewModel(
+                TestObjectFactory.getAllGooglePublishUseCase(googlePublishRepository),
+                TestObjectFactory.getAllRESTPublishUseCase(restPublishRepository),
+                TestObjectFactory.getUpdateGooglePublishUseCase(googlePublishRepository),
+                TestObjectFactory.getUpdateRESTPublishUseCase(restPublishRepository),
+                GooglePublishDataMapper(),
+                GooglePublishModelDataMapper(),
+                RESTPublishDataMapper(),
+                RESTPublishModelDataMapper()
+            )
+        }
+    }
+}
