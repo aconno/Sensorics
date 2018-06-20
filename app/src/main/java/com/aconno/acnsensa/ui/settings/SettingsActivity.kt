9f970546494ed317412342a9591be36b8732cd9b
Serhat <SerhatSurguvec@gmail.com>
2018-06-20 16:31:00 +0200

    Add Settings to Manifest

--- app/src/main/java/com/aconno/acnsensa/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/aconno/acnsensa/ui/settings/SettingsActivity.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.aconno.acnsensa.R
-import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
