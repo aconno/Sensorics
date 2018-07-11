8ba7a008ea6288f96ede34bd335bc643228e8df8
borna-mdn <borna@simvelop.de>
2018-07-11 11:13:03 +0200

    Add ScanDevice

--- domain/src/main/java/com/aconno/sensorics/domain/model/Device.kt
+++ domain/src/main/java/com/aconno/sensorics/domain/model/Device.kt
@@ -1,5 +1,7 @@
 package com.aconno.sensorics.domain.model
 
-import java.io.Serializable
-
-data class Device(val name: String, val macAddress: String, val icon: String = "") : Serializable
+data class Device(
+    val name: String,
+    val macAddress: String,
+    val icon: String
+)
