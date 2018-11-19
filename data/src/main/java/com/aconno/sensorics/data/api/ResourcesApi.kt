dcd891d4c576098522a3ae820757aa4899e12749
Serhat <SerhatSurguvec@gmail.com>
2018-11-19 10:29:03 +0100

    Add LatestVersionJsonModel into ResourcesApi

--- data/src/main/java/com/aconno/sensorics/data/api/ResourcesApi.kt
+++ data/src/main/java/com/aconno/sensorics/data/api/ResourcesApi.kt
@@ -1,5 +1,6 @@
 package com.aconno.sensorics.data.api
 
+import com.aconno.sensorics.data.repository.resources.LatestVersionJsonModel
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,7 +10,7 @@ import retrofit2.http.Query
 interface ResourcesApi {
 
     @GET("/sensorics/api/getLatestVersion.php")
-    fun getLatestVersion(@Query("version") version: Long): Call<String>
+    fun getLatestVersion(@Query("version") version: Long): Call<LatestVersionJsonModel>
 
 
     @GET("/sensorics/{filePath}")
