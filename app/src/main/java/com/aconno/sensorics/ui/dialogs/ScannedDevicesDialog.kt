b36947360659da06439406f453e08270cc3ef946
srishti77 <srishti.kristi12@gmail.com>
2018-11-21 14:38:13 +0100

    Added icon path in the DeviceSpinner

--- app/src/main/java/com/aconno/sensorics/ui/dialogs/ScannedDevicesDialog.kt
+++ app/src/main/java/com/aconno/sensorics/ui/dialogs/ScannedDevicesDialog.kt
@@ -20,7 +20,7 @@ import timber.log.Timber
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class ScannedDevicesDialog : DisposerDialogFragment() {
+class ScannedDevicesDialog() : DisposerDialogFragment() {
 
     @Inject
     lateinit var scanDeviceStream: Flowable<ScanDevice>
