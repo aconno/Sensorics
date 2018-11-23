5fc8818888c76c4ea69b46cc3eb0214f5401a471
Serhat <SerhatSurguvec@gmail.com>
2018-11-23 16:16:13 +0100

    Update Range for LiveGraph

--- data/src/main/java/com/aconno/sensorics/data/repository/resources/ResourceSyncerImpl.kt
+++ data/src/main/java/com/aconno/sensorics/data/repository/resources/ResourceSyncerImpl.kt
@@ -64,6 +64,6 @@ class ResourceSyncerImpl(
 
     companion object {
         const val LATEST_VERSION = "LATEST_VERSION"
-        const val LATEST_ASSETS_VERSION = 1542985227L
+        const val LATEST_ASSETS_VERSION = 1542986074L
     }
 }
