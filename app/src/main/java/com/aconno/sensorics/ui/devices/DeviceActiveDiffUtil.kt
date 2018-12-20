30274e307aa8f0abf185b8c8b3c27254d11bcc67
srishti77 <srishti.gaihre71@gmail.com>
2018-12-20 11:05:34 +0100

    Fix Rename Refresh problem

--- app/src/main/java/com/aconno/sensorics/ui/devices/DeviceActiveDiffUtil.kt
+++ app/src/main/java/com/aconno/sensorics/ui/devices/DeviceActiveDiffUtil.kt
@@ -11,6 +11,7 @@ class DeviceActiveDiffUtil : DiffUtil.ItemCallback<DeviceActive>() {
     override fun areContentsTheSame(p0: DeviceActive, p1: DeviceActive): Boolean {
         return !(p0.active.xor(p1.active)) &&
                 p0.device.name == p1.device.name &&
+                p0.device.alias == p1.device.alias &&
                 !(p0.device.connectable.xor(p1.device.connectable))
     }
 }
