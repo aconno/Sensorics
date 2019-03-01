a26b4e835a2427e43e12e5d2ab371399b1eb514f
Laert <laert@simvelop.de>
2019-03-01 15:59:00 +0100

    Created Beacon General Fragment

--- app/src/main/java/com/aconno/sensorics/ui/configure/BeaconGeneral2Fragment.kt
+++ app/src/main/java/com/aconno/sensorics/ui/configure/BeaconGeneral2Fragment.kt
@@ -1,29 +1,34 @@
 package com.aconno.sensorics.ui.configure
 
 
+import android.annotation.SuppressLint
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.aconno.bluetooth.beacon.Beacon
+import android.webkit.JavascriptInterface
+import com.aconno.bluetooth.beacon.BeaconInfo
 import com.aconno.sensorics.R
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_beacon_general2.*
-import timber.log.Timber
 
 
 class BeaconGeneral2Fragment : Fragment() {
 
+    var beaconInfo: BeaconInfo? = null
+    private var listener: BeaconGeneralFragmentListener? = null
+
+    private val beaconViewModel: BeaconViewModel by lazy {
+        ViewModelProviders.of(requireActivity()).get(BeaconViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        arguments.let {
-            val beacon = it?.getSerializable("OBJ") as? Beacon
-            beacon?.let {
-                Timber.d("Beacon is ${beacon.address}")
-            }
-        }
+        beaconInfo = BeaconInfo.Builder().build(beaconViewModel.beacon.value) //beaconViewModel.beacon.value
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_beacon_general2, container, false)
@@ -42,20 +47,58 @@ class BeaconGeneral2Fragment : Fragment() {
         webview_general.settings.allowUniversalAccessFromFileURLs = true
         webview_general.settings.allowContentAccess = true
         webview_general.loadUrl(HTML_FILE_PATH)
+        webview_general.addJavascriptInterface(this,"native")
+    }
+
+    @JavascriptInterface
+    fun retrieveCurrentBeaconInfo(): String {
+        beaconInfo?.let {
+            return Gson().toJson(beaconInfo)
+        }
+        return ""
+    }
+
+    @JavascriptInterface
+    fun updateFirmware(){
+        listener?.updateFirmware()
+    }
+
+    @JavascriptInterface
+    fun factoryReset(){
+        listener?.resetFactory()
+    }
+
+    @JavascriptInterface
+    fun addPassword(){
+        listener?.addPassword()
+    }
+
+    @JavascriptInterface
+    fun powerOff(){
+        listener?.powerOff()
+    }
+
+    @JavascriptInterface
+    fun changeConnectible(checked: Boolean){
+        beaconViewModel.beacon.value?.connectible = checked
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is BeaconGeneralFragmentListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnBeaconGeneralFragmentInteractionListener")
+        }
     }
 
     companion object {
-        //        const val HTML_FILE_PATH = "file:///android_asset/BeaconConfiguration/views/slot/Slot.html"
         const val HTML_FILE_PATH =
-            "file:///android_asset/BeaconConfiguration/views/arbitrary/ArbitraryData.html"
+            "file:///android_asset/BeaconConfiguration/views/general/General.html"
 
         @JvmStatic
-        fun newInstance(beacon: Beacon) =
-                BeaconGeneral2Fragment().apply {
-                    arguments = Bundle().apply {
-                        putSerializable("OBJ",beacon)
-                    }
-                }
+        fun newInstance() =
+                BeaconGeneral2Fragment()
     }
 
 
