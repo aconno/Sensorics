e152136916f9970fd3f49ad4e09206b0320c4fbd
Serhat <SerhatSurguvec@gmail.com>
2019-03-04 13:24:54 +0100

    Impl Hex Convertion

--- app/src/main/assets/resources/settings/views/slot/slot_init.js
+++ app/src/main/assets/resources/settings/views/slot/slot_init.js
@@ -3,7 +3,6 @@ let slot;
 let inited = false
 
 $(document).ready(function () {
-    
     if (inited) {
         return;
     }
@@ -129,6 +128,15 @@ $(document).ready(function () {
             });
 
             $('#custom_hex_enabled').change(function () {
+                let isHexEnabledButtonChecked = $('#custom_hex_enabled').is(":checked");
+                let sad = $('#custom_value').val();
+
+                if (isHexEnabledButtonChecked) {
+                    $('#custom_value').val(ascii_to_hexa(sad));
+                } else {
+                    $('#custom_value').val(hex_to_ascii(sad));
+                }
+
                 getUpdatedSlot();
             });
         }
@@ -192,6 +200,26 @@ $(document).ready(function () {
 
     inited = true;
 });
+
+//Function that converts ascii to hex
+function ascii_to_hexa(str) {
+    var arr1 = [];
+    for (var n = 0, l = str.length; n < l; n++) {
+        var hex = Number(str.charCodeAt(n)).toString(16);
+        arr1.push(hex);
+    }
+    return arr1.join('');
+}
+
+//Function that converts hex to ascii
+function hex_to_ascii(str1) {
+    var hex = str1.toString();
+    var str = '';
+    for (var n = 0; n < hex.length; n += 2) {
+        str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    }
+    return str;
+}
 
 function init(slotJson) {
     slot = JSON.parse(slotJson);
