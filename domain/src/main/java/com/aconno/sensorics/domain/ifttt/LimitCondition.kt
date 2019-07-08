7074ec7ce1eeb9dab1f2a8d0396ba576b98f1014
Roberto Anic Banic <rxa6313@rit.edu>
2019-07-08 12:44:54 +0200

    Removes the ability to select multiple conditions limiters Adds equal condition

--- domain/src/main/java/com/aconno/sensorics/domain/ifttt/LimitCondition.kt
+++ domain/src/main/java/com/aconno/sensorics/domain/ifttt/LimitCondition.kt
@@ -11,6 +11,7 @@ class LimitCondition(
             when (type) {
                 LESS_THAN -> input.value <= limit
                 MORE_THAN -> input.value >= limit
+                EQUAL_TO -> input.value == limit
                 else -> false
             }
         } else {
@@ -22,6 +23,7 @@ class LimitCondition(
         val sign = when (type) {
             LESS_THAN -> "<"
             MORE_THAN -> ">"
+            EQUAL_TO -> "="
             else -> throw IllegalArgumentException("Invalid constraint type: $type")
         }
         return "$readingType $sign $limit"
@@ -31,11 +33,13 @@ class LimitCondition(
 
         const val LESS_THAN = 0
         const val MORE_THAN = 1
+        const val EQUAL_TO = 2
 
         fun typeFromString(type: String): Int {
             return when (type) {
                 "<" -> LESS_THAN
                 ">" -> MORE_THAN
+                "=" -> EQUAL_TO
                 else -> throw IllegalArgumentException("Invalid constraint type: $type")
             }
         }
