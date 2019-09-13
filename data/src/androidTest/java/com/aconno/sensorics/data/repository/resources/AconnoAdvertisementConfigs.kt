dc3bf1dfcd6f31b5af493752b19e0b975877f845
grubelic <damjan.grubelic@gmail.com>
2019-09-13 13:50:21 +0200

    Added ResourceSyncerImpl test

--- data/src/androidTest/java/com/aconno/sensorics/data/repository/resources/AconnoAdvertisementConfigs.kt
+++ data/src/androidTest/java/com/aconno/sensorics/data/repository/resources/AconnoAdvertisementConfigs.kt
@@ -11,4 +11,15 @@ object AconnoAdvertisementConfigs {
         "device_screen_path" : "/sensorics/device_screens/acnsensa.html"
     }
     """.trimIndent()
+
+    val ACN_BEACON = """
+    {
+        "id" : "0201",
+        "name" : "acnBEACON",
+        "usecase_screen_path" : "/sensorics/usecase_screens/acnsensa.html",
+        "icon_path" : "/sensorics/icons/ic_sensa.png",
+        "format_path" : "/sensorics/formats/0201.json",
+        "device_screen_path" : "/sensorics/device_screens/empty.html"
+    }
+    """.trimIndent()
 }
