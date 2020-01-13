4e9e47c8bdf9429c6e345329d7f4da1082b05820
Giorgi Gusakov <giorgi@simvelop.de>
2020-01-13 13:24:53 +0100

    Add dialogs and snackBars. Handle unsuccessful use-cases

--- app/src/main/java/com/aconno/sensorics/ui/settings_framework/SettingsFrameworkActivity.kt
+++ app/src/main/java/com/aconno/sensorics/ui/settings_framework/SettingsFrameworkActivity.kt
@@ -1,5 +1,6 @@
 package com.aconno.sensorics.ui.settings_framework
 
+import android.app.Dialog
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
@@ -13,9 +14,9 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
-import com.aconno.sensorics.BluetoothConnectService
-import com.aconno.sensorics.R
+import com.aconno.sensorics.*
 import com.aconno.sensorics.device.beacon.Beacon
 import com.aconno.sensorics.device.beacon.v2.BeaconImpl
 import com.aconno.sensorics.device.bluetooth.BluetoothGattCallback
@@ -26,29 +27,36 @@ import com.aconno.sensorics.domain.model.GattCallbackPayload
 import com.aconno.sensorics.domain.scanning.Bluetooth
 import com.aconno.sensorics.domain.scanning.BluetoothTaskProcessor
 import com.aconno.sensorics.ui.configure.BeaconGeneralFragmentListener
+import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
 import dagger.android.support.DaggerAppCompatActivity
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_configure.*
+import kotlinx.android.synthetic.main.dialog_indeterminate_progress.*
 import timber.log.Timber
 import javax.inject.Inject
 
-class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCallback ,
+class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCallback,
     BeaconGeneralFragmentListener {
     private var bluetoothConnectService: BluetoothConnectService? = null
     private var connectResultDisposable: Disposable? = null
     private lateinit var macAddress: String
     private lateinit var taskProcessor: BluetoothTaskProcessor
     private lateinit var beacon: Beacon
+    private var isServiceBinded = false
 
     @Inject
     lateinit var beaconViewModel: BeaconSettingsViewModel
+
+    var progressDialog: Dialog? = null
+    var indefeniteSnackBar: Snackbar? = null
     var device: String = ""
 
     private val beaconSettingsPagerAdapter: BeaconSettingsPagerAdapter by lazy {
         BeaconSettingsPagerAdapter(supportFragmentManager).apply {
             beaconViewModel.beacon.observe(this@SettingsFrameworkActivity, Observer<Beacon> {
                 it?.let {
+                    tabs.visibility = View.VISIBLE
                     this.beacon = it
                 }
             })
@@ -56,16 +64,20 @@ class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCal
     }
 
     private val serviceConnection = object : ServiceConnection {
+
+
         override fun onServiceDisconnected(name: ComponentName?) {
             Timber.d("Disconnected")
             connectResultDisposable?.dispose()
             bluetoothConnectService = null
+            isServiceBinded = false
         }
 
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             bluetoothConnectService =
                 (service as? BluetoothConnectService.LocalBinder)?.getService()
             Timber.d("Connected")
+            isServiceBinded = true
 
             bluetoothConnectService?.let { bluetoothService ->
                 bluetoothService.getConnectResultsLiveData().observe(this@SettingsFrameworkActivity,
@@ -76,10 +88,18 @@ class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCal
 
                 taskProcessor = BluetoothTaskProcessorImpl(bluetoothService.bluetooth)
 
-                bluetoothService.startConnectionStream()
-
-                bluetoothService.connect(macAddress)
+                connectToDevice(bluetoothService)
             }
+        }
+    }
+
+    private fun connectToDevice(bluetoothService: BluetoothConnectService) {
+        bluetoothService.startConnectionStream()
+
+        bluetoothService.connect(macAddress)
+
+        progressDialog = showProgressDialog().apply {
+            message.text = "Connecting..."
         }
     }
 
@@ -88,17 +108,15 @@ class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCal
         setContentView(R.layout.activity_configure)
         getDeviceMacAddress(savedInstanceState)
 
-        if (intent.extras != null && intent.extras!!.containsKey(DEVICE_MAC_ADDRESS)) {
-            intent.extras!!.getString(DEVICE_MAC_ADDRESS)?.let {
-                device = it
-            }
-
-        } else {
-            throw IllegalArgumentException("Device not provided.")
-        }
+        device = intent?.extras?.getString(DEVICE_MAC_ADDRESS)
+            ?: throw throw IllegalArgumentException("Device not provided.")
 
         setSupportActionBar(toolbar)
 
+        setupUI()
+    }
+
+    private fun setupUI() {
         /*Set titlebar */
         supportActionBar?.subtitle = device
 
@@ -128,7 +146,8 @@ class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCal
 
     override fun onStop() {
         super.onStop()
-        unbindService(serviceConnection)
+        unbindServiceConnectionIfBounded()
+        indefeniteSnackBar?.dismiss()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -141,6 +160,7 @@ class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCal
         when (payload.action) {
             BluetoothGattCallback.ACTION_GATT_CONNECTED -> {
                 Timber.d("Connected to GATT")
+                progressDialog?.message?.text = "Connected. Unlocking..."
                 bluetoothConnectService?.let {
                     beacon = BeaconImpl(this, taskProcessor)
                 } ?: throw IllegalStateException(
@@ -150,11 +170,9 @@ class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCal
                 beacon.requestDeviceLockStatus(this)
             }
             BluetoothGattCallback.ACTION_GATT_DISCONNECTED -> {
-                Toast.makeText(
-                    this,
-                    "Disconnected: ${payload.payload}",
-                    Toast.LENGTH_LONG
-                ).show()
+                progressDialog?.dismiss()
+                Timber.d("Gatt disconnected: ${payload.payload}")
+                showTryAgainToConnectSnackBar()
             }
             else -> {
                 return
@@ -176,31 +194,76 @@ class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCal
                     beaconViewModel.beacon.value = beacon
                     tabs.visibility = View.VISIBLE
                 }
+
                 override fun execute(bluetooth: Bluetooth): Boolean {
                     return true
                 }
             })
         } else {
-            runOnUiThread {
-                createPasswordDialog(object : OnPasswordDialogAction {
-                    override fun onPasswordEntered(password: String) {
-                        beacon.unlock(password, this@SettingsFrameworkActivity) // TODO: Do this in a better way
-                    }
-
-                    override fun onDialogCancelled() {
-                        Toast.makeText(
-                            this@SettingsFrameworkActivity,
-                            "Cannot connect without a password!",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        unbindService(serviceConnection)
-                    }
-                }).show()
-            }
+            progressDialog?.apply {
+                message.text = "Need password"
+            }?.window?.decorView?.postDelayed({
+                progressDialog?.dismiss()
+                showPasswordDialog()
+            }, 1500)
         }
     }
 
     override fun onError(e: Exception) {
+    }
+
+    private fun showPasswordDialog() {
+        createPasswordDialog(object : OnPasswordDialogAction {
+            override fun onPasswordEntered(password: String) {
+                beacon.unlock(
+                    password,
+                    this@SettingsFrameworkActivity
+                ) // TODO: Do this in a better way
+                if (progressDialog?.isShowing == false) {
+                    progressDialog = showProgressDialog().apply {
+                        message.text = "Unlocking..."
+                    }
+                }
+            }
+
+            override fun onDialogCancelled() {
+                showTryAgainPasswordSnackBar()
+            }
+        }).show()
+    }
+
+    private fun showTryAgainPasswordSnackBar() {
+        indefeniteSnackBar = ll_settings_root.snack(
+            "Cannot connect without a password!",
+            Snackbar.LENGTH_INDEFINITE
+        ) {
+            action(
+                "try again",
+                ContextCompat.getColor(applicationContext, R.color.primaryColor)
+            ) {
+                showPasswordDialog()
+            }
+        }
+    }
+
+
+    private fun showTryAgainToConnectSnackBar() {
+        indefeniteSnackBar = ll_settings_root.snack("Error occurred", Snackbar.LENGTH_INDEFINITE) {
+            action(
+                "try again",
+                ContextCompat.getColor(applicationContext, R.color.primaryColor)
+            ) {
+                bluetoothConnectService?.let { connectToDevice(it) }
+                    ?: showToast("Oops. Can't do anything")
+            }
+        }
+    }
+
+    private fun unbindServiceConnectionIfBounded() {
+        if (isServiceBinded) {
+            unbindService(serviceConnection)
+            isServiceBinded = false;
+        }
     }
 
     private fun createPasswordDialog(onPasswordDialogAction: OnPasswordDialogAction): AlertDialog {
@@ -216,14 +279,33 @@ class SettingsFrameworkActivity : DaggerAppCompatActivity(), LockStateRequestCal
             onPasswordDialogAction.onDialogCancelled()
             dialog.dismiss()
         }
+        builder.setCancelable(false)
         builder.setOnCancelListener { onPasswordDialogAction.onDialogCancelled() }
         return builder.create()
+    }
+
+    private fun showProgressDialog(): AlertDialog {
+        return AlertDialog.Builder(this).apply {
+            setView(
+                layoutInflater.inflate(
+                    R.layout.dialog_indeterminate_progress,
+                    null as ViewGroup?
+                )
+            )
+        }.create().apply {
+            setCancelable(false)
+            setCanceledOnTouchOutside(false)
+        }.also {
+            if (!isFinishing and !isDestroyed) {
+                it.show()
+            }
+        }
     }
 
     private fun getDeviceMacAddress(savedInstanceState: Bundle?) {
         macAddress = intent?.getStringExtra(DEVICE_MAC_ADDRESS)
             ?: savedInstanceState?.getString(DEVICE_MAC_ADDRESS)
-                ?: ""
+                    ?: ""
 
         if (macAddress.isBlank()) {
             Timber.e("MAC address not passed to activity!")
