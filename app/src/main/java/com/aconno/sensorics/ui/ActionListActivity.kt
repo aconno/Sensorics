c73630cecf95bfc88efb227860aaf4a9084ab1b9
Roberto Anic Banic <rxa6313@rit.edu>
2020-04-10 12:54:34 +0200

    Action UI refactors

--- app/src/main/java/com/aconno/sensorics/ui/ActionListActivity.kt
+++ app/src/main/java/com/aconno/sensorics/ui/ActionListActivity.kt
@@ -12,23 +12,25 @@ import com.aconno.sensorics.R
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_action_list.*
 
-class ActionListActivity : DaggerAppCompatActivity(), ActionListFragment.ItemSelectionStateListener {
+class ActionListActivity : DaggerAppCompatActivity(),
+    ActionListFragment.ItemSelectionStateListener {
 
     private var mainMenu: Menu? = null
-    private var menuResource : Int = R.menu.share_all_menu
+    private var menuResource: Int = R.menu.share_all_menu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_action_list)
         actions_toolbar.title = getString(R.string.actions)
 
-        val fm = supportFragmentManager
-        if (fm != null) {
-            var fragment: Fragment? = fm.findFragmentById(R.id.action_list_container)
-            if (fragment == null) {
-                fragment = ActionListFragment.newInstance()
-                fm.beginTransaction().add(R.id.action_list_container, fragment).commit()
-            }
+        val fragment: Fragment? = supportFragmentManager.findFragmentById(
+            R.id.action_list_container
+        )
+        if (fragment == null) {
+            supportFragmentManager.beginTransaction().add(
+                R.id.action_list_container,
+                ActionListFragment.newInstance()
+            ).commit()
         }
 
         invalidateOptionsMenu()
@@ -38,11 +40,14 @@ class ActionListActivity : DaggerAppCompatActivity(), ActionListFragment.ItemSel
 
 
     override fun onItemSelectionStateEntered() {
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.let { actionBar ->
+            actionBar.setDisplayHomeAsUpEnabled(true)
 
-        val cancelIcon = getDrawable(R.drawable.ic_action_notify_cancel)
-        cancelIcon?.setColorFilter(Color.BLACK,PorterDuff.Mode.MULTIPLY)
-        supportActionBar?.setHomeAsUpIndicator(cancelIcon)
+            getDrawable(R.drawable.ic_action_notify_cancel)?.let { drawable ->
+                drawable.setColorFilter(Color.BLACK, PorterDuff.Mode.MULTIPLY) // TODO: Fix
+                actionBar.setHomeAsUpIndicator(drawable)
+            }
+        }
 
         menuResource = R.menu.share_selected_menu
         invalidateOptionsMenu()
@@ -57,9 +62,8 @@ class ActionListActivity : DaggerAppCompatActivity(), ActionListFragment.ItemSel
     }
 
     override fun onSelectedItemsCountChanged(selectedItems: Int) {
-        actions_toolbar.title = getString(R.string.selected_items_count,selectedItems)
+        actions_toolbar.title = getString(R.string.selected_items_count, selectedItems)
     }
-
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
         mainMenu = menu
@@ -68,21 +72,20 @@ class ActionListActivity : DaggerAppCompatActivity(), ActionListFragment.ItemSel
         return true
     }
 
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         (supportFragmentManager
-                .findFragmentById(R.id.action_list_container) as ActionListFragment)
-                .resolveActionBarEvent(item)
+            .findFragmentById(R.id.action_list_container) as ActionListFragment)
+            .resolveActionBarEvent(item)
 
         return super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed() {
         val actionListFragment = supportFragmentManager
-                .findFragmentById(R.id.action_list_container) as ActionListFragment
+            .findFragmentById(R.id.action_list_container) as ActionListFragment
 
         val handled = actionListFragment.onBackButtonPressed()
-        if(!handled) super.onBackPressed()
+        if (!handled) super.onBackPressed()
     }
 
 
