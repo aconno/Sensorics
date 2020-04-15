fd10df804e88866ae6f3d7f35bd57a3040f32c2a
Roberto Anic Banic <rxa6313@rit.edu>
2020-04-15 14:20:24 +0200

    Clean up UseCasesFragment

--- app/src/main/java/com/aconno/sensorics/ui/UseCasesFragment.kt
+++ app/src/main/java/com/aconno/sensorics/ui/UseCasesFragment.kt
@@ -9,20 +9,18 @@ import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
 import com.aconno.sensorics.LiveDataObserver
 import com.aconno.sensorics.R
 import com.aconno.sensorics.viewmodel.UseCasesViewModel
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_use_cases.*
-import timber.log.Timber
 import javax.inject.Inject
 
 
 class UseCasesFragment : DaggerFragment() {
 
     @Inject
-    lateinit var mViewModel: UseCasesViewModel
+    lateinit var useCasesViewModel: UseCasesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,13 +31,11 @@ class UseCasesFragment : DaggerFragment() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Timber.e("onViewCreated")
 
-        val macAddress = arguments?.getString(UseCasesFragment.USECASES_MAC_ADDRESS_EXTRA)
-        val name = arguments?.getString(UseCasesFragment.USECASES_NAME_EXTRA)
+        val macAddress = arguments?.getString(USECASES_MAC_ADDRESS_EXTRA)
+        val name = arguments?.getString(USECASES_NAME_EXTRA)
 
         if (macAddress != null && name != null) {
-
             activity_usecases_webview.apply {
                 webChromeClient = WebChromeClient()
                 webViewClient = MyWebViewClient()
@@ -49,43 +45,48 @@ class UseCasesFragment : DaggerFragment() {
                 settings.displayZoomControls = false
             }
 
-            mViewModel.url.observe(this, LiveDataObserver { loadUrl(it) })
-            mViewModel.urlError.observe(this, LiveDataObserver { showError() })
+            useCasesViewModel.url.observe(viewLifecycleOwner, LiveDataObserver {
+                loadUrl(it)
+            })
+            useCasesViewModel.urlError.observe(viewLifecycleOwner, LiveDataObserver {
+                showError()
+            })
 
-            mViewModel.mutableShowProgress.observe(this, LiveDataObserver { showProgressBar(it) })
-            mViewModel.mutableHideProgress.observe(this, LiveDataObserver { hideProgressBar(it) })
+            useCasesViewModel.mutableShowProgress.observe(viewLifecycleOwner, LiveDataObserver {
+                showProgressBar()
+            })
+            useCasesViewModel.mutableHideProgress.observe(viewLifecycleOwner, LiveDataObserver {
+                hideProgressBar()
+            })
 
-            mViewModel.initViewModel(macAddress, name)
+            useCasesViewModel.initViewModel(macAddress, name)
         }
     }
 
     private fun showError() {
-        context?.let {
-            val builder = AlertDialog.Builder(it)
-            builder.setMessage("There are no UseCase files defined.")
-                .setNeutralButton("Ok") { _, _ ->
-                    (context as AppCompatActivity).supportFragmentManager.popBackStack()
-                }
-
-            builder.create()
-                .show()
-        } ?: throw IllegalStateException("Activity cannot be null")
+        AlertDialog.Builder(requireContext())
+            .setMessage(R.string.no_use_case_files_defined)
+            .setNeutralButton(R.string.ok) { _, _ ->
+                requireActivity().supportFragmentManager.popBackStack()
+            }
+            .create()
+            .show()
     }
 
-    private fun hideProgressBar(it: Unit?) {
+    private fun hideProgressBar() {
         progressbar.visibility = View.GONE
         status_view.visibility = View.GONE
         activity_usecases_webview.visibility = View.VISIBLE
     }
 
-    private fun showProgressBar(it: Unit?) {
+    private fun showProgressBar() {
         progressbar.visibility = View.VISIBLE
         status_view.visibility = View.VISIBLE
         activity_usecases_webview.visibility = View.GONE
     }
 
     override fun onDetach() {
-        mViewModel.unsubscribe()
+        useCasesViewModel.unsubscribe()
         super.onDetach()
     }
 
@@ -94,10 +95,9 @@ class UseCasesFragment : DaggerFragment() {
     }
 
     inner class MyWebViewClient : WebViewClient() {
-
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
-            mViewModel.subscribe()
+            useCasesViewModel.subscribe()
         }
     }
 
@@ -106,12 +106,12 @@ class UseCasesFragment : DaggerFragment() {
         private const val USECASES_NAME_EXTRA = "name"
 
         fun newInstance(macAddress: String, name: String): UseCasesFragment {
-            val useCasesFragment = UseCasesFragment()
-            val bundle = Bundle()
-            bundle.putString(USECASES_MAC_ADDRESS_EXTRA, macAddress)
-            bundle.putString(USECASES_NAME_EXTRA, name)
-            useCasesFragment.arguments = bundle
-            return useCasesFragment
+            return UseCasesFragment().apply {
+                arguments = Bundle().apply {
+                    putString(USECASES_MAC_ADDRESS_EXTRA, macAddress)
+                    putString(USECASES_NAME_EXTRA, name)
+                }
+            }
         }
     }
 }
