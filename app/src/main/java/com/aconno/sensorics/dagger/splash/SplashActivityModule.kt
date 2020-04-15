ff14f21d2a8526459729d7e9f69b9c11e2149761
Roberto Anic Banic <rxa6313@rit.edu>
2020-04-15 14:06:17 +0200

    Inject ViewModel through Dagger for SplashActivity

--- app/src/main/java/com/aconno/sensorics/dagger/splash/SplashActivityModule.kt
+++ app/src/main/java/com/aconno/sensorics/dagger/splash/SplashActivityModule.kt
@@ -1,8 +1,11 @@
 package com.aconno.sensorics.dagger.splash
 
+import androidx.lifecycle.ViewModelProvider
 import com.aconno.sensorics.SensoricsApplication
 import com.aconno.sensorics.data.repository.ResourcesInitializerImpl
 import com.aconno.sensorics.domain.ResourcesInitializer
+import com.aconno.sensorics.ui.SplashActivity
+import com.aconno.sensorics.viewmodel.SplashViewModel
 import com.aconno.sensorics.viewmodel.factory.SplashViewModelFactory
 import dagger.Module
 import dagger.Provides
@@ -14,8 +17,17 @@ class SplashActivityModule {
     @SplashActivityScope
     fun provideSplashViewModelFactory(
         resourcesInitializer: ResourcesInitializer
-    ): SplashViewModelFactory =
-        SplashViewModelFactory(resourcesInitializer)
+    ): SplashViewModelFactory = SplashViewModelFactory(resourcesInitializer)
+
+    @Provides
+    @SplashActivityScope
+    fun provideSplashViewModel(
+        activity: SplashActivity,
+        splashViewModelFactory: SplashViewModelFactory
+    ): SplashViewModel = ViewModelProvider(
+        activity,
+        splashViewModelFactory
+    ).get(SplashViewModel::class.java)
 
     @Provides
     @SplashActivityScope
--- app/src/main/java/com/aconno/sensorics/ui/SplashActivity.kt
+++ app/src/main/java/com/aconno/sensorics/ui/SplashActivity.kt
@@ -3,10 +3,8 @@ package com.aconno.sensorics.ui
 import android.content.Intent
 import android.os.Bundle
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.aconno.sensorics.R
 import com.aconno.sensorics.viewmodel.SplashViewModel
-import com.aconno.sensorics.viewmodel.factory.SplashViewModelFactory
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
@@ -14,16 +12,11 @@ import javax.inject.Inject
 class SplashActivity : DaggerAppCompatActivity() {
 
     @Inject
-    lateinit var splashViewModelFactory: SplashViewModelFactory
-
-    private lateinit var splashViewModel: SplashViewModel
+    lateinit var splashViewModel: SplashViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        splashViewModel = ViewModelProviders.of(this, splashViewModelFactory)
-            .get(SplashViewModel::class.java)
 
         splashViewModel.initializationLiveEvent.observe(this, Observer {
             val intent = Intent(this, MainActivity::class.java)
