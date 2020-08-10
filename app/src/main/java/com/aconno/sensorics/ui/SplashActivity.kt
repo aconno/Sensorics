850decf4a07deb4f37c2bb7d6d38345699cf8e6a
Pascal <pascal@simvelop.de>
2020-08-10 15:39:11 +0200

    Swap deprecated ViewModelProviders.of with viewModels delegate

--- app/src/main/java/com/aconno/sensorics/ui/SplashActivity.kt
+++ app/src/main/java/com/aconno/sensorics/ui/SplashActivity.kt
@@ -2,8 +2,8 @@ package com.aconno.sensorics.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.aconno.sensorics.R
 import com.aconno.sensorics.viewmodel.SplashViewModel
 import com.aconno.sensorics.viewmodel.factory.SplashViewModelFactory
@@ -16,14 +16,11 @@ class SplashActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var splashViewModelFactory: SplashViewModelFactory
 
-    private lateinit var splashViewModel: SplashViewModel
+    private val splashViewModel by viewModels<SplashViewModel> { splashViewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        splashViewModel = ViewModelProviders.of(this, splashViewModelFactory)
-            .get(SplashViewModel::class.java)
 
         splashViewModel.initializationLiveEvent.observe(this, Observer {
             val intent = Intent(this, MainActivity::class.java)
