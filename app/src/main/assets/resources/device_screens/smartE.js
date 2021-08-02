507a7119f9fc93b22315bcda8deea5852e36e1d5
roberto.ab <roberto@aconno.de>
2021-08-02 08:24:00 +0200

    SmartE

--- app/src/main/assets/resources/device_screens/smartE.js
+++ app/src/main/assets/resources/device_screens/smartE.js
@@ -40,50 +40,148 @@ function setPower(power_in_watts) {
 }
 
 window.onload = function () {
-    chart = new CanvasJS.Chart("chartContainer", {
-        theme: "light1", // "light1", "light2", "dark1", "dark2"
-        animationEnabled: true,
-        zoomEnabled: true,
-        title: {
-            text: "Energy & Power Chart"
-        },
-        axisY: [{
-            title: "Energy",
-            suffix: " kWh",
-            titleFontColor: ENERGY_COLOR,
-            labelFontColor: ENERGY_COLOR
-        }],
-        axisY2: [{
-            title: "Power",
-            suffix: " kW",
-            titleFontColor: POWER_COLOR,
-            labelFontColor: POWER_COLOR
-        }],
-        data: [{
-            axisYIndex: 0,
-            type: "splineArea",
-            yValueFormatString: '#0.#',
-            color: ENERGY_COLOR,
-            xValueType: "dateTime",
-            xValueFormatString: "DD MMM YY HH:mm",
-            legendMarkerType: "square",
-            dataPoints: []
-        }, {
-            axisYIndex: 1,
-            type: "column",
-            axisYType: "secondary",
-            yValueFormatString: '#0.#',
-            color: POWER_COLOR,
-            xValueType: "dateTime",
-            xValueFormatString: "DD MMM YY HH:mm",
-            legendMarkerType: "square",
-            dataPoints: []
-        }]
-    });
+    // chart = new CanvasJS.Chart("chartContainer", {
+    //     theme: "light1", // "light1", "light2", "dark1", "dark2"
+    //     animationEnabled: true,
+    //     zoomEnabled: true,
+    //     title: {
+    //         text: "Energy & Power Chart"
+    //     },
+    //     axisY: [{
+    //         title: "Energy",
+    //         suffix: " kWh",
+    //         titleFontColor: ENERGY_COLOR,
+    //         labelFontColor: ENERGY_COLOR
+    //     }],
+    //     axisY2: [{
+    //         title: "Power",
+    //         suffix: " kW",
+    //         titleFontColor: POWER_COLOR,
+    //         labelFontColor: POWER_COLOR
+    //     }],
+    //     data: [{
+    //         axisYIndex: 0,
+    //         type: "splineArea",
+    //         yValueFormatString: '#0.#',
+    //         color: ENERGY_COLOR,
+    //         xValueType: "dateTime",
+    //         xValueFormatString: "DD MMM YY HH:mm",
+    //         legendMarkerType: "square",
+    //         dataPoints: []
+    //     }, {
+    //         axisYIndex: 1,
+    //         type: "column",
+    //         axisYType: "secondary",
+    //         yValueFormatString: '#0.#',
+    //         color: POWER_COLOR,
+    //         xValueType: "dateTime",
+    //         xValueFormatString: "DD MMM YY HH:mm",
+    //         legendMarkerType: "square",
+    //         dataPoints: []
+    //     }]
+    // });
+    //
+    // addDataPoints(100);
+    //
+    // chart.render();
+    //
+    // function addDataPoints(noOfDps) {
+    //     let xVal = new Date(), yVal = 100, yVal2 = 100;
+    //
+    //     for (let i = 0; i < noOfDps; i++) {
+    //         yVal += Math.random() * 100;
+    //         yVal2 = 100 + Math.random() * 100;
+    //         chart.options.data[0].dataPoints.push({x: xVal, y: yVal});
+    //         chart.options.data[1].dataPoints.push({x: xVal, y: yVal2});
+    //         xVal = new Date(xVal.valueOf() + 864E5);
+    //     }
+    // }
+
+    let data = {
+        // labels: [],
+        datasets: [
+            {
+                label: 'Energy',
+                data: [],
+                borderColor: ENERGY_COLOR,
+                backgroundColor: ENERGY_COLOR + "80",
+                yAxisID: 'y',
+            },
+            {
+                label: 'Power',
+                data: [],
+                borderColor: POWER_COLOR,
+                backgroundColor: POWER_COLOR + "80",
+                yAxisID: 'y1',
+            }
+        ]
+    }
+
+    chart = new Chart(document.getElementById('myChart').getContext('2d'), {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            interaction: {
+                mode: 'index',
+                intersect: false,
+            },
+            stacked: false,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Chart.js Line Chart - Multi Axis'
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(context) {
+                            return `${context.dataset.label}: ${(context.parsed.y / 1000).toFixed(3)} ${context.datasetIndex === 0 ? 'kWh' : "kW"}`;
+                        }
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        displayFormats: {
+                            quarter: 'MMM YYYY'
+                        }
+                    }
+                },
+                y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+
+                    ticks: {
+                        callback: function(value, index, values) {
+                            return `${(value / 1000).toFixed(2)} kWh`;
+                        }
+                    }
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+
+                    ticks: {
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return `${(value / 1000).toFixed(2)} kW`;
+                        }
+                    },
+
+                    grid: {
+                        drawOnChartArea: false, // only want the grid lines for one axis to show up
+                    },
+                },
+            }
+        }
+    })
+
 
     addDataPoints(100);
-
-    chart.render();
 
     function addDataPoints(noOfDps) {
         let xVal = new Date(), yVal = 100, yVal2 = 100;
@@ -91,12 +189,17 @@ window.onload = function () {
         for (let i = 0; i < noOfDps; i++) {
             yVal += Math.random() * 100;
             yVal2 = 100 + Math.random() * 100;
-            chart.options.data[0].dataPoints.push({x: xVal, y: yVal});
-            chart.options.data[1].dataPoints.push({x: xVal, y: yVal2});
+            // chart.data.labels.push(xVal);
+            chart.data.datasets[0].data.push({x: xVal, y: yVal});
+            chart.data.datasets[1].data.push({x: xVal, y: yVal2});
             xVal = new Date(xVal.valueOf() + 864E5);
         }
+
+        chart.update()
     }
 
     energyElement = document.getElementById("energy_value")
     powerElement = document.getElementById("power_value")
+
+
 }
